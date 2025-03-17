db4b031cf9b7f1d67eccf8115f1892a70e933a90
dangkhoaphamvu <phamvudangkhoa0802@gmail.com>
2025-03-17 16:22:44 +0700

    First commit

---
+++ URLShortener_BackEnd/URLShortener_BackEnd/Controllers/UrlShortenerController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using URLShortener_BackEnd.Models;
+using URLShortener_BackEnd.Services;
+
+namespace URLShortener_BackEnd.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class UrlShortenerController : ControllerBase
+    {
+        private readonly UrlShortenerService _urlShortenerService;
+
+        public UrlShortenerController(UrlShortenerService urlShortenerService)
+        {
+            _urlShortenerService = urlShortenerService;
+        }
+
+        [HttpPost("shorten")]
+        public async Task<IActionResult> ShortenUrl([FromBody] ShortUrl request)
+        {
+            if (!Uri.IsWellFormedUriString(request.OriginalUrl, UriKind.Absolute))
+            {
+                return BadRequest("Invalid URL format.");
+            }
+
+            var shortUrl = await _urlShortenerService.CreateShortUrlAsync(request.OriginalUrl);
+            return Ok(shortUrl);
+        }
+
+        [HttpGet("{shortCode}")]
+        public async Task<IActionResult> RedirectToOriginalUrl(string shortCode)
+        {
+            var urlEntry = await _urlShortenerService.GetByShortCodeAsync(shortCode);
+            if (urlEntry == null)
+            {
+                return NotFound("Short URL not found.");
+            }
+
+            return Redirect(urlEntry.OriginalUrl);
+        }
+    }
+}
---
+++ URLShortener_BackEnd/URLShortener_BackEnd/Services/UrlShortenerService.cs
@@ -0,0 +1,95 @@
+﻿﻿using MongoDB.Driver;
+using URLShortener_BackEnd.Models;
+using Microsoft.Extensions.Options;
+using System.Text;
+using System.Security.Cryptography;
+using StackExchange.Redis;
+
+namespace URLShortener_BackEnd.Services
+{
+    public class UrlShortenerService
+    {
+        private readonly IMongoCollection<ShortUrl> _urlsCollection;
+        private readonly IDatabase _redisDb;
+
+        public UrlShortenerService(IOptions<MongoDbSetting> mongoDbSettings, IConfiguration config)
+        {
+            var mongoClient = new MongoClient(mongoDbSettings.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(mongoDbSettings.Value.DatabaseName);
+            _urlsCollection = mongoDatabase.GetCollection<ShortUrl>(mongoDbSettings.Value.CollectionName);
+
+            var redis = ConnectionMultiplexer.Connect($"{config["Redis:Host"]}:{config["Redis:Port"]}");
+            _redisDb = redis.GetDatabase();
+        }
+
+        public async Task<ShortUrl?> GetByShortCodeAsync(string shortUrl)
+        {
+            try
+            {
+                if (_redisDb.Multiplexer.IsConnected)
+                {
+                    var cachedUrl = await _redisDb.StringGetAsync(shortUrl);
+                    if (!string.IsNullOrEmpty(cachedUrl))
+                    {
+                        return new ShortUrl { ShortCode = shortUrl, OriginalUrl = cachedUrl };
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("[Redis Warning] Unable to connect Redis, using MongoDB.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Redis Error] Unable to read data from cache: {ex.Message}");
+            }
+            var url = await _urlsCollection.Find(u => u.ShortCode == shortUrl).FirstOrDefaultAsync();
+            if (url != null)
+            {
+                try
+                {
+                    if (_redisDb.Multiplexer.IsConnected)
+                    {
+                        await _redisDb.StringSetAsync(shortUrl, url.OriginalUrl, TimeSpan.FromHours(24));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"[Redis Error] Unable to write data to cache: {ex.Message}");
+                }
+            }
+            return url;
+        }
+
+        public async Task<ShortUrl> CreateShortUrlAsync(string originalUrl)
+        {
+            var existingUrl = await _urlsCollection.Find(url => url.OriginalUrl == originalUrl).FirstOrDefaultAsync();
+            if (existingUrl != null)
+            {
+                return existingUrl;
+            }
+
+            var shortCode = GenerateShortCode(originalUrl);
+            var shortUrl = new ShortUrl
+            {
+                OriginalUrl = originalUrl,
+                ShortCode = shortCode
+            };
+
+            await _urlsCollection.InsertOneAsync(shortUrl);
+            return shortUrl;
+        }
+
+
+        private string GenerateShortCode(string url)
+        {
+            using var sha256 = SHA256.Create();
+            var hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(url));
+            return Convert.ToBase64String(hash)
+                .Substring(0, 7)
+                .Replace("/", "_")
+                .Replace("+", "-");
+        }
+    }
+}
